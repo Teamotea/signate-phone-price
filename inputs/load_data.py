bc89ea53e85e57420a7f0b5ef9802b912860f8af
temote <i.temote315@gmail.com>
2021-05-30 16:41:31 +0900

    Add load_train_data and load_test_data functions

--- inputs/load_data.py
+++ inputs/load_data.py
@@ -2,6 +2,20 @@ import os
 import pandas as pd
 
 
+def load_train_data():
+    dir_name = os.path.dirname(os.path.abspath(__file__))
+    train = pd.read_csv(dir_name + '/train.csv')
+    train_x = train.drop('price_range', axis=1)
+    train_y = train['price_range']
+    return train_x, train_y
+
+
+def load_test_data():
+    dir_name = os.path.dirname(os.path.abspath(__file__))
+    test_x = pd.read_csv(dir_name + '/test.csv')
+    return test_x
+
+
 def load_data():
     dir_name = os.path.dirname(os.path.abspath(__file__))
     train = pd.read_csv(dir_name + '/train.csv')
